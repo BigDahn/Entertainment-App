d5c48aeae88a02088999a719b83f518e6ded5479
Big Dahn <harkinborholarholuwah@gmail.com>
2025-06-22 15:54:02 +0100

    movie ui updated

--- src/Movie/MovieDetails.jsx
+++ src/Movie/MovieDetails.jsx
@@ -29,7 +29,7 @@ function MovieDetails() {
   }
 
   return (
-    <main className="px-4 w-full">
+    <main className="px-4 w-full overflow-y-scroll">
       <div className="flex w-full justify-end  mt-2 ">
         <Button
           style="bg-gray-50 shadow-sm px-3 py-2 flex items-center gap-2 cursor-pointer"
@@ -81,7 +81,7 @@ function MovieDetails() {
                     <div className="grid grid-cols-3 gap-2">
                       {stars?.map((s) => {
                         return (
-                          <h3 className="bg-gray-50 rounded-sm py-1 text-[15px] font-normal px-2 shadow-md">
+                          <h3 className="bg-gray-500 rounded-sm py-1 text-[15px] font-normal px-2 shadow-md">
                             {s}
                           </h3>
                         );
@@ -95,7 +95,20 @@ function MovieDetails() {
                   )}
                 </div>
 
-                <div>rating</div>
+                <div className="flex items-start font-semibold bg-blue-600 py-3 rounded-sm text-[20px] text-white justify-center gap-1">
+                  <h3>Movie Rating:</h3>
+                  <div className="w-7 h-7">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="#fcc419"
+                      stroke="#fcc419"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+                  <h3 className="text-[12px]">{rating}/10</h3>{" "}
+                </div>
                 <div className="flex gap-2 bg-blue-600 py-3 text-white rounded-sm font-semibold text-[20px] w-full justify-center">
                   <h1 className="font-semibold "> Director:</h1>
                   <h3>{director}</h3>
